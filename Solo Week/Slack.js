3a68408842011944d7c8032b501e440932c98440
Anthony Kim <akim2214@gmail.com>
2020-03-29 22:21:11 -0700

    [prog] removed email addresses

--- Solo Week/Slack.js
+++ Solo Week/Slack.js
@@ -1,5 +1,5 @@
-var names = ['Alexander Benko', 'Andrew More', 'Anna Dinh', 'Christopher Sanchez', 'Frankie Barreto', 'Frankie Liu', 'Jeremy Ho', 'John Kim', 'Jordan Chun', 'Joshua Clute', 'Kyle Shelton', 'Marlena Ajoste', 'Michael Siu', 'Mrinal Vig', 'Omar El Tarzi', 'Rongyi Du', 'Rylan Albach', 'Stephanie Shively', 'Tayo Omololu', 'Vai San (Danny)', 'Wilson Papilla'];
-var emails = ['alexbenko@live.com', 'recondomore@gmail.com', 'annathuydinh@gmail.com', 'chrissanchez89@gmail.com', 'barretoacquire@gmail.com', 'zhuangfrankie@gmail.com', 'eneka.ken@gmail.com', 'jkim005@yahoo.com', 'jchun765@gmail.com', 'clutejoshua@gmail.com', 'kylejamesshelton@gmail.com', 'mpajoste@gmail.com', 'michaeltsiu13@gmail.com', 'mrinalvig@hotmail.com', 'omartarzi@gmail.com', 'rongyi.du@gmail.com', 'rylanalbach@gmail.com', 'smshively16@gmail.com', 'gokeomololu@gmail.com', 'vai.quoc.san@gmail.com', 'wilson.pap@gmail.com']
+var names = [];
+var emails = []
 
 function addEmailToSlack (emailsArr, namesArr){
   var result = [];
